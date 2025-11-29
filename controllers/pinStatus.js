325aa306fd5f80853fb936d74d0b61b2f1c8308c
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-11-29 23:36:27 +0100

    Create pinStatus.js

---
+++ controllers/pinStatus.js
@@ -0,0 +1,12 @@
+import pinSchemas from "../models/PinModel.js";
+
+export const PinStatus = async (req, res) =>{
+  if (!req.user) {
+    return res.status(400).json({message:"User Not Found"});
+  }
+  const user = await pinSchemas.findOne({user: req.user.id});
+  if (user) {
+    return res.status(200).json({status:false});
+  }
+  return res.status(200).json({status: true});
+}
