3afba9aff4415a5b4370b283b05d3c06e471a993
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-11-29 23:38:17 +0100

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { body } from "express-validator";
-
+import { PinSetUp } from "../controllers/pinSetController.js";
+import { PinStatus } from "../controllers/pinStatus.js";
 import { myProfile } from "../controllers/profileController.js";
 import { fetchBalance } from "../controllers/fetchBalanceController.js";
 import { PinUpdate } from "../controllers/pinUpdateController.js";
@@ -19,11 +20,10 @@ import { VerifyNameController } from "../controllers/cableVerifyControlller.js";
 
 const authRoute = express.Router();
 
-// Profile & Balance
 authRoute.get("/profile", verifyToken, myProfile);
 authRoute.get("/balance", verifyToken, fetchBalance);
 
-// Data Plans & Transactions
+
 authRoute.get("/dataplan", verifyToken, getAllDataPlans);
 authRoute.post("/data/purchase", [body("phoneNumber")
   .isString()
@@ -33,15 +33,16 @@ authRoute.post("/data/purchase", [body("phoneNumber")
       .isInt({ min: 1 }).withMessage("serviceID must be a positive integer")], verifyToken, BuyBundle);
 authRoute.get("/transaction", verifyToken, getUserTransactions);
 
-// Account Management
 authRoute.post("/account/generate", verifyToken, createController);
 authRoute.get("/fetch/details", verifyToken, getUserAccount);
 
-// PIN Verification
+
 authRoute.get("/verify/pin", verifyToken, verifyController);
+authRoute.post("/pin/set-up", verifyToken, PinSetUp);
+authRoute.get("/pin/status", verifyToken, PinStatus);
 
 authRoute.post("/update-pin", verifyToken, PinUpdate);
-// Cable
+
 authRoute.post("/verify/cable", [body("provider").isString(), body("cardNumber")
     .exists().withMessage("Smartcard number is required")
     .isNumeric().withMessage("Smartcard number must be numeric")
@@ -49,7 +50,6 @@ authRoute.post("/verify/cable", [body("provider").isString(), body("cardNumber")
 authRoute.get("/cable/plans", verifyToken, getCable);
 authRoute.post("/cable/subscribe", verifyToken, CablePayment);
 
-// Airtime
 authRoute.post("/airtime/purchase",[body("network").isString(),body("phoneNumber")
   .isString()
   .matches(/^0[7-9][0-1]\d{8}$/)
@@ -59,13 +59,11 @@ authRoute.post("/airtime/purchase",[body("network").isString(),body("phoneNumber
     .custom((value) => value > 0).withMessage("Amount must be greater than 0")
     .custom((value) => value <= 10000).withMessage("Amount cannot exceed 10,000")], verifyToken, AirtimePurchase);
 
-// Webhook
 authRoute.post(
   "/payvessel/webhook",
   express.json({ verify: (req, res, buf) => (req.rawBody = buf) }),
   payvesselWebhook
 );
 
-// Dangerous admin route (should be removed)
 
 export default authRoute;
