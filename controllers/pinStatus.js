0d616cda13393c4844e8b459d5ffd8abdaedca98
sulaimonazeez <olaniyisulaimon221@gmail.com>
2026-01-01 20:10:25 -0600

    committed

--- controllers/pinStatus.js
+++ controllers/pinStatus.js
@@ -1,6 +1,8 @@
 import pinSchemas from "../models/PinModel.js";
 
 export const PinStatus = async (req, res) =>{
+  res.set("Cache-Control", "no-store");
+
   if (!req.user) {
     return res.status(400).json({message:"User Not Found"});
   }
